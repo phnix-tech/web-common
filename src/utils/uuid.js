b44242598e18a1d9095c8ec4f308e7900c89a7fa
leejefy <050934@gmail.com>
2020-09-20 00:15:14 +0800

    getURLParameter/uuid util function

---
+++ src/utils/uuid.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string|undefined} prefix
+ * @returns {string}
+ * @desc get uuid, borrowed from `gs-common/fe/Functions@uuid`
+ */
+function uuid (prefix) {
+  prefix = prefix || "";
+  const d = new Date().getTime();
+  const str = prefix + "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
+  return (
+    str.replace(/[xy]/g, c => {
+      const r = (d + Math.random() * 16) % 16 | 0;
+      return (c === "x" ? r : (r & 0x3 | 0x8)).toString(16);
+    })
+  );
+}
+
+export default uuid;
