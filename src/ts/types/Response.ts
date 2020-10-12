4d0bbdbf0979fd06dea79abb6b5410f74189a997
leejefy <050934@gmail.com>
2020-10-12 18:11:36 +0800

    define http RawData type

--- src/ts/types/Response.ts
+++ src/ts/types/Response.ts
@@ -24,7 +24,20 @@ export interface PageResponse<T = Any> {
   page?: Page;
 }
 
+/**
+ * http lib raw response, it's lib dependent
+ * see `AxiosResponse` - https://github.com/axios/axios#response-schema
+ * `jqXHR` Object - https://api.jquery.com/jquery.ajax/
+ * `fetch` Response - https://developer.cdn.mozilla.net/en-US/docs/Web/API/Fetch_API
+ */
 export type RawResponse<T = Any> = T;
+
+/**
+ * server response raw data, it's commonly defined as json format
+ * `{data: any, msg: string, status: number}`
+ * `{data: any, message: string, success: boolean}`
+ */
+export type RawData<T = Any> = T;
 
 type Response = ObjectResponse | ArrayResponse | PageResponse;
 
