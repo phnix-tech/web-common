b2f08e3ee3851c147cfb8b222a233069927161e9
leejefy <050934@gmail.com>
2020-10-24 18:12:22 +0800

    create local debug js in build process

--- build/build.js
+++ build/build.js
@@ -1,6 +1,6 @@
+const fs = require("fs");
+const path = require("path");
 const
-  fs = require("fs"),
-  path = require("path"),
   // 相对路径都以项目根路径为基准
   // build output dir
   origResPath = "./dist",
@@ -18,7 +18,7 @@ let
 
 /**
  * clean up build output files
- * 默认下删除`./dist`目录
+ * 默认删除`./dist`目录
  * @param {string|Array<string>} paths
  * 额外删除的目录路径，路径为相对路径，相对于项目根路径
  */
@@ -58,10 +58,35 @@ function copyResources () {
   _logging.info("==============copy resource files DONE==============");
 }
 
+/**
+ * 创建本地调试js/ts文件，本地调试文件会被版本管理系统排除
+ * 可用于修改前端环境变量比如切换api服务器实时调试而不用重启服务器等
+ */
+function createLocalDebugJs () {
+  const debugJsPath = path.join(_dirname, "./src/debug.js");
+  const localDebugJsPath = path.join(_dirname, "./src/debug.local.js");
+  const debugTsPath = path.join(_dirname, "./src/debug.ts");
+  const localDebugTsPath = path.join(_dirname, "./src/debug.local.ts");
+
+  _logging.info("createLocalDebugJs");
+  if (fs.existsSync(debugJsPath) && !fs.existsSync(localDebugJsPath)) {
+    _logging.debug("copy", debugJsPath, "to", localDebugJsPath);
+    fs.copyFileSync(debugJsPath, localDebugJsPath);
+  }
+  if (fs.existsSync(debugTsPath) && !fs.existsSync(localDebugTsPath)) {
+    _logging.debug("copy", debugTsPath, "to", localDebugTsPath);
+    fs.copyFileSync(debugTsPath, localDebugTsPath);
+  }
+}
+
 function setup ({
+  // optional
   fse,
+  // required
   logging,
+  // required
   functions,
+  // required
   dirname
 }) {
   _fse = fse;
@@ -75,5 +100,6 @@ module.exports = {
   setup,
   clean,
   copyCommonModules,
-  copyResources
+  copyResources,
+  createLocalDebugJs
 };
