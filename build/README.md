5d32d4580b852dee49d23e6aa72af38dfcec5677
leejefy <050934@gmail.com>
2020-10-30 21:56:40 +0800

    move build scripts to web build repositoty

---
+++ build/README.md
@@ -0,0 +1,3 @@
+# @web/build
+
+> 构建脚本已经转移到新仓库，请访问[http://gitlab.k8s.owinfo.net/web/build/tree/master/scripts](http://gitlab.k8s.owinfo.net/web/build/tree/master/scripts)
--- build/web-build.js
+++
@@ -1,8 +0,0 @@
-const logging = require("@web-io/build/fe/Logging");
-const functions = require("@web-io/build/functions");
-
-// web builds external deps
-module.exports = {
-  logging,
-  functions
-};
