9c05190e53ccef173d3ec2b068c0ae9b9aa32368
leejefy <050934@gmail.com>
2021-02-20 23:46:14 +0800

    output ts declaration files

---
+++ lib/types/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * `any` alias, 用于统一关闭`any` lint错误
+ */
+export declare type Any = any;
+/**
+ * 空接口类型，用于泛型默认空对象参数，或者限定泛型参数为对象类型
+ */
+export interface EmptyObject {
+}
---
+++ lib/utils/http/addUrlTimestamp.d.ts
@@ -0,0 +1,6 @@
+/**
+ * add timestamp to url (GET method only) for disable cache
+ * @param {string} url
+ * @returns {string}
+ */
+export default function (url?: string): string;
