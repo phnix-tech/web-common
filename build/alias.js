0da4ae2b7d8b6fcefccdb90d68752330a3c08382
leejefy <050934@gmail.com>
2021-03-22 18:15:07 +0800

    chore: 统一web common和web build resolve函数

--- build/alias.js
+++ build/alias.js
@@ -1,24 +1,6 @@
-const path = require("path");
+const resolve = require("./resolve");
 
 /**
- * 基于项目根解析路径
- * git子模块引用：假设当前路径为`src/common/build`
- * npm包引用：假设当前路径`node_modules/@web-io/common/build`
- *
- * @param {string[]} dir
- * @returns
- */
-function resolve (...dir) {
-  let projectRoot = path.join(__dirname, "../../..");
-  // npm包引用
-  if (path.basename(projectRoot) === "node_modules") {
-    projectRoot = path.join(projectRoot, "..");
-  }
-  return path.join(projectRoot, ...dir);
-}
-
-/**
- *
  * webpack别名定义，用于IDE智能提示
  * `@` 定位到项目根目录下的`src`目录
  */
