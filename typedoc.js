36d63777d67afcea042ab540d0aad0d93d70d8c5
leejefy <050934@gmail.com>
2021-07-28 22:52:13 +0800

    chore: migrate typedoc to @web-io/tsdoc 1.0.0-dev1
    `typedoc`模块可能会被安装到`@web-io/tsdoc`下面的`node_modules`中，我们自己定义`web-io-tsdoc`命令

--- typedoc.js
+++ typedoc.js
@@ -1,13 +1,3 @@
-/**
- * @see http://typedoc.org/guides/options/#configuration-options
- */
-module.exports = {
-  includeVersion: true,
-  entryPoints: ["./src"],
-  // 排除测试文件
-  exclude: [
-    "**/*+(.test|.spec).ts",
-    "**/*+(.test|.spec).tsx"
-  ],
-  out: "docs"
-};
+const {defineConfig} = require("@web-io/tsdoc");
+
+module.exports = defineConfig();
