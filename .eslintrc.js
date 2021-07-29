8c6c73a4f97d38f13c46f92d50754d0c2c1686c1
li.zhihui35 <li.zhihui35@iwhalecloud.com>
2021-07-29 10:01:45 +0800

    chore: upgrade @web-io/lint to 1.0.1-dev1
    move eslint rule no-trailing-spaces to @web-io/lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,5 @@
 const cfg = require("@web-io/lint/eslint/eslint-ts");
 cfg.rules = Object.assign(cfg.rules || {}, {
   // add or override rules
-  "no-trailing-spaces": [
-    "error",
-    {
-      ignoreComments: true
-    }
-  ]
 });
 module.exports = cfg;
